6a31d34f1786686111d144f3d90d92d217d51529
DanielElisenberg <daniel@elisenberg.com>
2021-12-01 07:13:31 +0100

    day01: completed

--- day01/day01.py
+++ day01/day01.py
@@ -1,21 +1,20 @@
-def rate_of_increase(depth_list: list) -> int:
+def increasing_measurements(depth_list: list) -> int:
     return sum([
-        previous_depth < current_depth for previous_depth, current_depth 
+        previous_depth < current_depth for previous_depth, current_depth
         in zip(depth_list, depth_list[1:])
     ])
+
 
 if __name__ == '__main__':
     with open('day01/input') as input_file:
         depth_list = [int(line.strip('\n')) for line in input_file.readlines()]
 
-    first_solution = rate_of_increase(depth_list)
+    first_solution = increasing_measurements(depth_list)
 
-    three_measurement_sliding_window = [
-        first_measurement + second_measurement + third_measurement
-        for first_measurement, second_measurement, third_measurement
-        in zip(depth_list, depth_list[1:], depth_list[2:])
-    ]
-    second_solution = rate_of_increase(three_measurement_sliding_window)
+    three_measurement_sliding_window = list(
+        map(sum, zip(depth_list, depth_list[1:], depth_list[2:]))
+    )
+    second_solution = increasing_measurements(three_measurement_sliding_window)
 
     print(
         f"""day 1:
---
+++ day02/day02.py
@@ -0,0 +1,4 @@
+
+if __name__ == "__main__":
+    with open('day02/input') as input_file:
+        input = [int(line.strip('\n')) for line in input_file.readlines()]
