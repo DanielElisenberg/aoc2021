9d8f1a851c9bf2538041ac4a69d9a91d7ca3cf46
DanielElisenberg <>
2021-12-02 08:06:43 +0100

    day02: complete

--- day02/day02.py
+++ day02/day02.py
@@ -1,4 +1,39 @@
+import numpy as np
+
+
+def resolve_position_part_one(actions):
+    direction_map = {
+        "forward": np.array([1, 0]),
+        "down": np.array([0, 1]),
+        "up": np.array([0, -1])
+    }
+    my_position = sum(
+        [direction_map[action[0]] * int(action[1]) for action in actions]
+    )
+    return my_position[0]*my_position[1]
+
+
+def resolve_position_part_two(actions):
+    my_position = np.array([0, 0])
+    aim = 0
+    for (direction, amount) in actions:
+        if direction in "up":
+            aim -= int(amount)
+        if direction == "down":
+            aim += int(amount)
+        if direction == "forward":
+            my_position += np.array([int(amount), int(amount)*aim])
+    return my_position[0] * my_position[1]
+
 
 if __name__ == "__main__":
     with open('day02/input') as input_file:
-        input = [int(line.strip('\n')) for line in input_file.readlines()]
+        actions = [
+            line.strip('\n').split(" ") for line in input_file.readlines()
+        ]
+
+    print(
+        f"""Day 2:
+        first solution: {resolve_position_part_one(actions)}
+        second solution: {resolve_position_part_two(actions)}"""
+    )
---
+++ day03/day03.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    with open('day03/input') as input_file:
+        actions = [line for line in input_file.readlines()]
