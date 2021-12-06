20058f44639bf69a593ca4ae4fd52254b024487c
DanielElisenberg <33904479+DanielElisenberg@users.noreply.github.com>
2021-12-06 06:33:13 +0100

    day06: completed

--- day06/day06.py
+++ day06/day06.py
@@ -1,3 +1,31 @@
+from collections import defaultdict
+
+
+def run_simulation(lanternfish: dict, generations: int) -> int:
+    for _ in range(generations):
+        next_generation = defaultdict(int)
+        for internal_timer, amount in lanternfish.items():
+            if internal_timer == 0:
+                next_generation[6] += amount
+                next_generation[8] += amount
+            else:
+                next_generation[internal_timer-1] += amount
+        lanternfish = next_generation
+    return sum(amount for amount in lanternfish.values())
+
+
 if __name__ == "__main__":
     with open('day06/input') as input_file:
-        input = [line.strip() for line in input_file.readlines()]
+        fish_list = [
+            int(internal_timer)
+            for internal_timer in input_file.read().split(",")
+        ]
+    lanternfish = defaultdict(int)
+    for internal_timer in fish_list:
+        lanternfish[internal_timer] += 1
+
+    print(
+        f"""Day 6:
+        first solution: {run_simulation(lanternfish, generations=80)}
+        second solution: {run_simulation(lanternfish, generations=256)}"""
+    )
