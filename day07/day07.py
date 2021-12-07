44634d072ace257ad3b34236db08f69ba0dc9528
DanielElisenberg <33904479+DanielElisenberg@users.noreply.github.com>
2021-12-07 06:31:59 +0100

    day07: completed

--- day07/day07.py
+++ day07/day07.py
@@ -1,14 +1,24 @@
 if __name__ == "__main__":
     with open('day07/input') as input_file:
-        input = [
-            line.strip().split() for line in input_file.readlines()
+        crab_positions = [
+            int(number) for number in input_file.read().split(",")
         ]
 
-    first_solution = 0
-    second_solution = 0
+    fuel_usage_constant = []
+    fuel_usage_summed = []
+
+    for align_at in range(min(crab_positions), max(crab_positions) + 1):
+        constant_fuel = 0
+        summed_fuel = 0
+        for crab_position in crab_positions:
+            diff = abs(align_at - crab_position)
+            constant_fuel += diff
+            summed_fuel += diff * (diff + 1) // 2
+        fuel_usage_constant.append(constant_fuel)
+        fuel_usage_summed.append(summed_fuel)
 
     print(
         f"""Day 7:
-        first solution: {first_solution}
-        second solution: {second_solution}"""
+        first solution: {min(fuel_usage_constant)}
+        second solution: {min(fuel_usage_summed)}"""
     )
