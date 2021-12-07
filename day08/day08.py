ca501397548cf27fe7db8298a7849da05624d68f
DanielElisenberg <33904479+DanielElisenberg@users.noreply.github.com>
2021-12-07 06:34:28 +0100

    day07: prepare day08

---
+++ day08/day08.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    with open('day08/input') as input_file:
+        input = [
+            line.strip() for line in input_file.readlines()
+        ]
+
+    first_solution = 0
+    second_solution = 0
+
+    print(
+        f"""Day 8:
+        first solution: {first_solution}
+        second solution: {second_solution}"""
+    )
