58e1eec7591af3d8a98d0865bd2ce47d71a0d09c
DanielElisenberg <33904479+DanielElisenberg@users.noreply.github.com>
2021-12-10 07:11:53 +0100

    day10: prepared day11

---
+++ day11/day11.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    with open('day11/input') as input_file:
+        input = [
+            line.strip() for line in input_file.readlines()
+        ]
+
+    first_solution = []
+    second_solution = []
+    print(
+        f"""Day 11:
+        first solution: {first_solution}
+        second solution: {second_solution}"""
+    )
