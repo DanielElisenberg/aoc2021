bad344f424ed00da0d7dcd108d26ee0bf75bfa19
DanielElisenberg <33904479+DanielElisenberg@users.noreply.github.com>
2021-12-10 07:09:33 +0100

    day10: completed

--- day10/day10.py
+++ day10/day10.py
@@ -1,14 +1,61 @@
+from typing import Tuple
+
+
+def completion_points_for_line(open_chunks: list) -> int:
+    point_for = {
+        "(": 1,
+        "[": 2,
+        "{": 3,
+        "<": 4
+    }
+    missing_characters = [
+        character for character in list(reversed(open_chunks))
+    ]
+    completion_points = 0
+    for character in missing_characters:
+        completion_points = completion_points * 5 + point_for[character]
+    return completion_points
+
+
+def parse_line(line: str) -> Tuple[list, int]:
+    open_chunks = []
+    match_for = {
+        "[": "]",
+        "(": ")",
+        "{": "}",
+        "<": ">"
+    }
+    for character in line:
+        if character in ["(", "[", "{", "<"]:
+            open_chunks.append(character)
+        else:
+            if match_for[open_chunks.pop()] != character:
+                syntax_error_score = {
+                    ")": 3,
+                    "]": 57,
+                    "}": 1197,
+                    ">": 25137
+                }[character]
+                return open_chunks, syntax_error_score
+    return open_chunks, 0
+
+
 if __name__ == "__main__":
+    syntax_error_score = 0
+    completion_points = []
+
     with open('day10/input') as input_file:
-        input = [
-            line.strip() for line in input_file.readlines()
-        ]
-
-    first_solution = []
-    second_solution = []
-
+        for line in input_file.readlines():
+            open_chunks, line_syntax_error_score = parse_line(line.strip())
+            if line_syntax_error_score:
+                syntax_error_score += line_syntax_error_score
+            else:
+                completion_points.append(
+                    completion_points_for_line(open_chunks)
+                )
+    completion_points.sort()
     print(
         f"""Day 10:
-        first solution: {first_solution}
-        second solution: {second_solution}"""
+        first solution: {syntax_error_score}
+        second solution: {completion_points[(len(completion_points))//2]}"""
     )
