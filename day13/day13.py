a64b624f0ee4f2e9a14f039269b22ae9e2c21533
DanielElisenberg <33904479+DanielElisenberg@users.noreply.github.com>
2021-12-12 14:04:45 +0100

    day12: prepared day13

---
+++ day13/day13.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    with open('day13/input') as input_file:
+        input = [
+            line.strip() for line in input_file.readlines()
+        ]
+
+    print(
+        f"""Day 13:
+        first solution: {0}
+        second solution: {0}"""
+    )
