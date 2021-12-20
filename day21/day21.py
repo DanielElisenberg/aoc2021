d4aae5d445e62b26fdfdcb07e76a94cd2c75b52a
DanielElisenberg <33904479+DanielElisenberg@users.noreply.github.com>
2021-12-20 20:52:08 +0100

    day20: prepare day21

---
+++ day21/day21.py
@@ -0,0 +1,12 @@
+if __name__ == "__main__":
+    with open('day21/input') as input_file:
+        lines = [
+            line.strip() for line in
+            input_file.readlines()
+        ]
+
+    print(
+        f"""Day 21:
+        first solution: {0}
+        second solution: {0}"""
+    )
